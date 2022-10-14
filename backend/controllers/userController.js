d40678a0239336892200ca2539c997167d1e492b
Joshi Vivek <vivek.joshi@tietoevry.com>
2022-10-14 20:54:52 +0530

    update profile and review

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/UserModel");
 const { hashPassword, comparePasswords } = require("../utils/hashPassword");
 const generateAuthToken = require("../utils/generateAuthToken");
+const Review = require("../models/ReviewModel");
 
 const getUsers = async (req, res, next) => {
   try {
@@ -103,9 +104,72 @@ const loginUser = async (req, res, next) => {
   }
 };
 
-const updateUserProfile = async(req, res, next) => {
+const updateUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).orFail();
+    user.name = req.body.name || user.name;
+    user.lastName = req.body.lastName || user.lastName;
+    user.email = req.body.email || user.email;
+    user.phoneNumber = req.body.phoneNumber;
+    user.address = req.body.address;
+    user.country = req.body.country;
+    user.zipCode = req.body.zipCode;
+    user.city = req.body.city;
+    user.state = req.body.state;
+    if (req.body.password !== user.password) {
+      user.password = hashPassword(req.body.password);
+    }
+    await user.save();
 
+    res.json({
+      success: "user updated",
+      userUpdated: {
+        _id: user._id,
+        name: user.name,
+        lastName: user.lastName,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getUserProfile = async(req, res, next) => {
+try {
+  const user = await User.findById(req.params.id).orFail();
+  return res.send(user)
+} catch (error) {
+  next(err);
+}
 }
 
-module.exports = { getUsers, registerUser, loginUser };
+const writeReview = async (req, res, next) => {
+  try {
+    const {comment, rating} = req.body
+
+    if(!(comment && rating)) {
+      return res.status(400).send("All inputs are required") 
+    }
+
+    const ObjectId = require("mongodb").ObjectId;
+
+    let reviewId = ObjectId()
+
+    await Review.create([
+      {
+        _id:reviewId,
+        comment: comment,
+        rating:Number(rating),
+        user:{_id:req.user._id, name:req.user.name + " " + req.user.lastName}
+      }
+    ])
+    res.send("review created")
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { getUsers, registerUser, loginUser, updateUserProfile, getUserProfile, writeReview };
 
